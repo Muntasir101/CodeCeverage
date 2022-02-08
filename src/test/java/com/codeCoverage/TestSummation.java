843a46ce62df9f20dc0bac8071cb022f9be57e9b
Muntasir Abdullah Mizan <muntasir.abdullah01@gmail.com>
2022-02-08 13:06:12 +0600

    git commit -m "Test Case added"

--- src/test/java/com/codeCoverage/TestSummation.java
+++ src/test/java/com/codeCoverage/TestSummation.java
@@ -14,4 +14,20 @@ public class TestSummation {
         Summation obj=new Summation();
         Assertions.assertEquals(55,obj.doSum(25,30));
     }
+
+    @Test
+    public void TC_003(){
+        Summation obj=new Summation();
+        Assertions.assertEquals(20,obj.doSum(-10,30));
+    }
+    @Test
+    public void TC_004(){
+        Summation obj=new Summation();
+        Assertions.assertEquals(-5,obj.doSum(25,-30));
+    }
+    @Test
+    public void TC_005(){
+        Summation obj=new Summation();
+        Assertions.assertEquals(-55,obj.doSum(-25,-30));
+    }
 }
